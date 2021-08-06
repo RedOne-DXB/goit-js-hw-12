5ca47326e09e6efb3372563969409d4cb2bf7e3f
Rodion Romanenko <rod.romanenko@gmail.com>
2021-08-06 11:43:10 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,3 +2,11 @@ import './css/styles.css';
 import './js/fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+
+fetch('https://restcountries.eu/rest/v2/name/united').then(response => {
+    return response.json();
+}).then(country => {
+    console.log(country);
+}).catch(error => {
+    console.log(error);
+});
