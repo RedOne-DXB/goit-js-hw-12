83657b78e7a73d5b4aaaba5132b51f1d92e8f808
Rodion Romanenko <rod.romanenko@gmail.com>
2021-08-06 16:05:15 +0400

    markup ready, polishing of styles left

--- src/index.js
+++ src/index.js
@@ -1,20 +1,45 @@
 import './css/styles.css';
 import API from './js/fetchCountries';
-import countriesTemplate from './templates/countries.hbs';
+import countryCardTemplate from './templates/countryCard.hbs';
+import countryListCardTemplate from './templates/countryListCard';
 
+const debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
 
 const refs = {
     countryList: document.querySelector('.country-list'),
     countryInfo: document.querySelector('.country-info'),
+    searchBox: document.querySelector('#search-box'),
 }
 
+refs.searchBox.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
-API.fetchCountries('suriname')
+function onInput(evt) {
+    refs.countryList.innerHTML = '';
+    refs.countryInfo.innerHTML = '';
+    const searchKey = evt.target.value;
+    if (searchKey.trim('') === '') {
+        return;
+    }
+console.log(searchKey);
+
+
+    API.fetchCountries(searchKey)
     .then(renderCountryCard)
     .catch(error => console.log(error));
+}
 
 function renderCountryCard(country) {
-    const markup = countriesTemplate(country[0]);
+    if (country.length === 1) {
+         const markup = countryCardTemplate(country[0]);
     refs.countryInfo.innerHTML = markup;
+    } else if (country.length >= 2 && country.length < 10) {
+        const listMarkup = countryListCardTemplate(country)
+        refs.countryList.innerHTML = listMarkup;
+    } else if (country.status === 404) {
+        console.log('Such a country NOT FOUND');
+    } else if (country.length > 10) {
+        console.log('Too many countries')
+    }
+   
 }
