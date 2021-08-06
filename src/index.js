49dd9f328f074512b0bc9377eef54cbf8fd5ea26
Rodion Romanenko <rod.romanenko@gmail.com>
2021-08-06 12:42:37 +0400

    first country fetched

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
+import { mark } from 'regenerator-runtime';
 import './css/styles.css';
 import './js/fetchCountries';
+import countriesTemplate from './templates/countries.hbs';
 
 const DEBOUNCE_DELAY = 300;
 
-fetch('https://restcountries.eu/rest/v2/name/united').then(response => {
+const refs = {
+    countryList: document.querySelector('.country-list'),
+    countryInfo: document.querySelector('.country-info'),
+}
+
+fetch('https://restcountries.eu/rest/v2/name/switzerland').then(response => {
     return response.json();
 }).then(country => {
     console.log(country);
+    const markup = countriesTemplate(country[0]);
+    refs.countryInfo.innerHTML = markup;
 }).catch(error => {
     console.log(error);
 });
